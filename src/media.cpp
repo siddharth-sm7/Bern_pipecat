b0067e689e41499bd40054fd578293a3c8b1c842
Aleix Conchillo Flaqué <aleix@daily.co>
2025-06-30 10:43:14 -0700

    src: media.cpp clang-formatting

--- src/media.cpp
+++ src/media.cpp
@@ -1,13 +1,13 @@
 #include <bsp/esp-bsp.h>
 #include <opus.h>
-#include <cstring>
+
 #include <atomic>
+#include <cstring>
 
 #include "esp_log.h"
-
 #include "main.h"
 
-#define SAMPLE_RATE     (16000)
+#define SAMPLE_RATE (16000)
 
 #define OPUS_BUFFER_SIZE 1276  // 1276 bytes is recommended by opus_encode
 #define PCM_BUFFER_SIZE 640
@@ -28,7 +28,6 @@ void set_is_playing(int16_t *in_buf, size_t in_samples) {
   is_playing = any_set;
 }
 
-
 esp_codec_dev_handle_t mic_codec_dev = NULL;
 esp_codec_dev_handle_t spk_codec_dev = NULL;
 
@@ -40,9 +39,9 @@ void oai_init_audio_capture() {
   esp_codec_dev_set_out_vol(spk_codec_dev, 255);
 
   esp_codec_dev_sample_info_t fs = {
-    .bits_per_sample = 16,
-    .channel = 1,
-    .sample_rate = SAMPLE_RATE,
+      .bits_per_sample = 16,
+      .channel = 1,
+      .sample_rate = SAMPLE_RATE,
   };
   esp_codec_dev_open(mic_codec_dev, &fs);
   esp_codec_dev_open(spk_codec_dev, &fs);
@@ -64,11 +63,14 @@ void oai_init_audio_decoder() {
 
 void oai_audio_decode(uint8_t *data, size_t size) {
   esp_err_t ret;
-  int decoded_size = opus_decode(opus_decoder, data, size, decoder_buffer, PCM_BUFFER_SIZE, 0);
+  int decoded_size =
+      opus_decode(opus_decoder, data, size, decoder_buffer, PCM_BUFFER_SIZE, 0);
 
   if (decoded_size > 0) {
     set_is_playing(decoder_buffer, decoded_size);
-    if ((ret = esp_codec_dev_write(spk_codec_dev, decoder_buffer, decoded_size * sizeof(uint16_t))) != ESP_OK) {
+    if ((ret = esp_codec_dev_write(spk_codec_dev, decoder_buffer,
+                                   decoded_size * sizeof(uint16_t))) !=
+        ESP_OK) {
       ESP_LOGE(TAG, "esp_codec_dev_write failed: %s", esp_err_to_name(ret));
     }
   }
@@ -80,13 +82,15 @@ uint8_t *read_buffer = NULL;
 
 void oai_init_audio_encoder() {
   int encoder_error;
-  opus_encoder = opus_encoder_create(SAMPLE_RATE, 1, OPUS_APPLICATION_VOIP, &encoder_error);
+  opus_encoder = opus_encoder_create(SAMPLE_RATE, 1, OPUS_APPLICATION_VOIP,
+                                     &encoder_error);
   if (encoder_error != OPUS_OK) {
     printf("Failed to create OPUS encoder");
     return;
   }
 
-  if (opus_encoder_init(opus_encoder, SAMPLE_RATE, 1, OPUS_APPLICATION_VOIP) != OPUS_OK) {
+  if (opus_encoder_init(opus_encoder, SAMPLE_RATE, 1, OPUS_APPLICATION_VOIP) !=
+      OPUS_OK) {
     printf("Failed to initialize OPUS encoder");
     return;
   }
@@ -95,12 +99,14 @@ void oai_init_audio_encoder() {
   opus_encoder_ctl(opus_encoder, OPUS_SET_COMPLEXITY(OPUS_ENCODER_COMPLEXITY));
   opus_encoder_ctl(opus_encoder, OPUS_SET_SIGNAL(OPUS_SIGNAL_VOICE));
 
-  read_buffer = (uint8_t *)heap_caps_malloc(PCM_BUFFER_SIZE, MALLOC_CAP_DEFAULT);
+  read_buffer =
+      (uint8_t *)heap_caps_malloc(PCM_BUFFER_SIZE, MALLOC_CAP_DEFAULT);
   encoder_output_buffer = (uint8_t *)malloc(OPUS_BUFFER_SIZE);
 }
 
 void oai_send_audio(PeerConnection *peer_connection) {
-  if (esp_codec_dev_read(mic_codec_dev, read_buffer, PCM_BUFFER_SIZE) != ESP_OK) {
+  if (esp_codec_dev_read(mic_codec_dev, read_buffer, PCM_BUFFER_SIZE) !=
+      ESP_OK) {
     printf("esp_codec_dev_read failed");
     return;
   }
@@ -109,6 +115,9 @@ void oai_send_audio(PeerConnection *peer_connection) {
     memset(read_buffer, 0, PCM_BUFFER_SIZE);
   }
 
-  auto encoded_size = opus_encode(opus_encoder, (const opus_int16* )read_buffer, PCM_BUFFER_SIZE / sizeof(uint16_t), encoder_output_buffer, OPUS_BUFFER_SIZE);
-  peer_connection_send_audio(peer_connection, encoder_output_buffer, encoded_size);
+  auto encoded_size = opus_encode(opus_encoder, (const opus_int16 *)read_buffer,
+                                  PCM_BUFFER_SIZE / sizeof(uint16_t),
+                                  encoder_output_buffer, OPUS_BUFFER_SIZE);
+  peer_connection_send_audio(peer_connection, encoder_output_buffer,
+                             encoded_size);
 }
